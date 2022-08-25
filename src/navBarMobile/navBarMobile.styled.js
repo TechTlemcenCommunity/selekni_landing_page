ef794ef6569cfe2f4654c33883fc5f2ed22d0e41
Dahmane Abdelmalek <dahmane.abdelmalek13@gmail.com>
2022-08-25 22:12:34 +0100

    update

--- src/navBarMobile/navBarMobile.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const NavBarMobileContainer = styled.div`
-  background-color: var(--main-yellow);
-  color: var(--man-black);
-  height: 60px;
-  display: ${(props) => (props.open ? "flex" : "none")}; ;
-`;
-export { NavBarMobileContainer };
